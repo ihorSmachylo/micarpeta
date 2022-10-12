8be97e9b8cd03f8ae09437e71b4e0fa0342d17ec
ihorSmachylo <115466660+ihorSmachylo@users.noreply.github.com>
2022-10-12 19:04:32 +0200

    Add files via upload

---
+++ 12.10. ejercico java/challenges.js
@@ -0,0 +1,234 @@
+/*
+1)
+console.log imprimirà qualsevol cosa que
+hi hagi entre els parèntesis a la teva consola.
+
+Canvia la frase a imprimir de sota, desa el fitxer,
+i veuràs els canvis al teu navegador.
+
+Després de confirmar que funciona, comenta"l.
+*/
+
+console.log("hello me");
+
+/*
+2)
+Fent servir tant l'operador (+) com l'operador (*),
+multiplica i afegeix qualsevol combinació de números
+per aconseguir imprimir el número 10.
+*/
+
+// console.log()
+
+/*
+3)
+Sovint fem servir console.log per fer `debug` (depurar errors).
+Pots afegir diversos arguments a dins del parèntesi del
+console.log (separant-los amb comes).
+Afegeix el següent dins el console.log!
+
+3 + 4, " should equal 7"
+*/
+
+// console.log()
+
+/*
+4)
+Afegeix dos `strings` per imprimir
+el teu nom i cognom conjuntament!
+*/
+
+// console.log()
+
+/*
+5)
+L'operador `modulo` (%) ens retorna la resta
+d'una divisió. Si dividim qualsevol número entre 2
+podrem saber si és parell o senar. Prova-ho!
+*/
+
+// console.log()
+
+/*
+6)
+Canvia la ubicació de Mia reassignant la variable
+"currentLocation" amb un nou string!
+Per fer-ho, hauràs d"afegir una nova línia de codi
+(no eliminis cap de les línies de codi).
+Nota: també hauràs de "descomentar" el console.log
+per imprimir la localització.
+*/
+
+const name = "Mia";
+let currentLocation = "school";
+
+// console.log(name, " is currently at ", currentLocation);
+
+/*
+7)
+Crea variables per que el missatge s'imprimeixi
+correctament a la consola!
+*/
+
+// console.log(person, " went to the ", place, " to eat ", food, ".");
+
+/*
+8)
+Ara, reassigna totes les variables anteriors per
+imprimir un nou missatge a la consola!
+*/
+
+// console.log(person, " went to the ", place, " to eat ", food, ".");
+
+/*
+9)
+Existeixen tres formes d'incrementar
+el número emmagatzemat a la variable "count".
+Tot i així, hi ha un problema amb la forma en què
+aquesta variable ha estat declarada. Corregeix-la!
+*/
+
+// const count = 0;
+// count = count + 1;
+// count += 1;
+// count++;
+
+// console.log(count, " should be 3");
+
+/*
+10)
+Hora de permutar! Al codi de sota volem intercanviar
+els valors emmagatzemats a "a" i "b".
+
+El codi no funciona correctament. Per assolir aquesta
+tasca hauràs de crear una variable adicional
+(pots anomenar-la "temp") per desar "a" o "b" mentre
+fas la permuta. Fes-te un esquema si ho necessites!
+*/
+
+let a = 1;
+let b = 2;
+
+// console.log("BEFORE > a is: ", a, " - and b is: ", b);
+
+// a = b;
+// b = a;
+
+// console.log("AFTER > a is: ", a, " - and b is: ", b);
+
+/*
+11)
+Fes que la següent afirmació resulti "true"!
+*/
+
+// console.log(3 > 4);
+
+/*
+12)
+Fes que la següent afirmació resulti "false".
+*/
+
+let x = 10;
+let y = x++;
+
+// console.log(x >= y);
+
+/*
+13)
+Soluciona l'operador de comparació per què
+l'afirmació següent resulti "false".
+*/
+
+// console.log(3 == "3");
+
+/*
+14)
+Modifica l'expressió de sota per què
+no resulti "false".
+*/
+
+const sunny = true;
+const warm = true;
+
+// console.log(sunny != warm);
+
+/*
+15)
+Practica tot el que has après!
+
+Inventa 10 expressions que facin servir el que
+has après fins ara.
+  - Fer servir els següents data types: `string`, `number`, `boolean`, `undefined` i `null`.
+  - Fer servir els següents operadors: (+) (-) (\*) (/) (%).
+  - Fer servir les paraules clau: var, const, let.
+  - Fer servir els operadors: (=) (+=) (-=) (*=)(++) (- -).
+  - Fer servir els operadors: (>) (<) (>=) (<=) (== vs ===) (!==).
+*/
+
+
+// ejercico en casa
+//1:Escribe un programa que muestre en pantalla “Hola” seguido de tu nombre guardado en una variable.//*
+let miNombre =" ihor"
+let saludo = "hola"
+//console.log(miNombre)
+// EJERCICO 2 Escribe un programa que pida al usuario un número y convierta el string a un int y lo muestre en pantalla
+
+//let numero1 = parseInt(prompt("dime un número"));
+//console.log(prompt+numero1)
+
+//EJERCICIO3 Escribe un programa que muestre la suma de dos números.
+
+let suma1 =3 ; let suma2 = 4;
+//console.log(suma1+suma2)
+
+//EJERCICIO 4 Escribe un programa que calcule la división de dos números y lo muestre en pantalla
+
+//console.log(suma2/suma1)
+
+// ejerciocio 5 pantalla5.Escribe  unprograma  que  pida  dos  números  al  usuario  y  muestre  el  resultado  de multiplicar los dos
+0
+//let numero1 = parseInt(prompt("dime un número"));
+//let numero2 = parseInt(prompt("dime otro número"));
+
+//console.log(numero1*numero2)
+
+// EJERCICIO 6 Escribe un programa que muestre el resultado de las siguientes operaciones:
+
+//console.log((-5) + 8 * 6)
+//console.log((55 + 9) % 6)
+//console.log( 20 + (-3*5) / 8)
+//console.log ((5 + 15) / (3 * 2) - (8 % 3))
+//console.log ((25.5 * 3.5) - (3.5 * 3.5)) / (40.5 - 4.5)
+//console.log (4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/1)))
+
+// ejercicio 7
+
+//let numero1 = parseInt(prompt("dime un número"));
+//let numero2 = parseInt(prompt("dime otro número"));
+
+//console.log(((numero1+numero2))*(numero1*numero2)-(numero1-numero2)/(numero1+numero2))
+
+// Ejercicio 8
+
+//let numero1 = parseInt(prompt("dime un número"));
+//console.log(numero1*=1)
+//console.log(numero1*=2)
+//console.log(numero1*=3)
+//console.log(numero1*=4)
+//console.log(numero1*=5)
+//console.log(numero1*=6)
+//console.log(numero1*=7)
+//console.log(numero1*=8)
+//console.log(numero1*=9)
+//console.log(numero1*=10)
+
+// ejercicio 9 
+
+
+// ejercicio 10 Escribe  un  programa  que  pida  tres  números al  usuario  y  que  calcule  y  muestre  su media.
+
+
+//let numero1 = parseInt(prompt("dime un número"));
+//let numero2 = parseInt(prompt("dime otro número"));
+//let numero3 = parseInt(prompt("dime otro número"));
+//console.log(prom= numero1+numero2+numero3)
